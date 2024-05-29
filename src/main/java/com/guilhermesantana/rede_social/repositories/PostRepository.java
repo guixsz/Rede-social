6e6dbbb83d8ecbbfefaa38f3f40628490a014dc7
Guilherme Santana <guiguilsantana@gmail.com>
2024-05-28 22:45:50 -0300

    feat: Created PostRepository

---
+++ src/main/java/com/guilhermesantana/rede_social/repositories/PostRepository.java
@@ -0,0 +1,14 @@
+package com.guilhermesantana.rede_social.repositories;
+
+import com.guilhermesantana.rede_social.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, UUID> {
+
+    Optional<Post> findPostById(UUID id);
+}
