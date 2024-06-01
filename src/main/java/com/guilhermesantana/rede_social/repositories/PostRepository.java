9fafd206f51686abb71e6187eb3c06a0aa76e199
Guilherme Santana <guiguilsantana@gmail.com>
2024-06-01 19:16:26 -0300

    feat: Added findPostsByUserId in PostRepository

--- src/main/java/com/guilhermesantana/rede_social/repositories/PostRepository.java
+++ src/main/java/com/guilhermesantana/rede_social/repositories/PostRepository.java
@@ -4,6 +4,7 @@ import com.guilhermesantana.rede_social.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,4 +12,6 @@ import java.util.UUID;
 public interface PostRepository extends JpaRepository<Post, UUID> {
 
     Optional<Post> findPostById(UUID id);
+
+    Optional<List<Post>> findPostsByUserId(UUID id);
 }
