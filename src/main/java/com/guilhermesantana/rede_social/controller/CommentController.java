b4d7439e80c7b14855447fc1668909a2e1de6759
Guilherme Santana <guiguilsantana@gmail.com>
2024-06-02 17:13:27 -0300

    feat: Added method findAllComments in CommentRepository

--- src/main/java/com/guilhermesantana/rede_social/controller/CommentController.java
+++ src/main/java/com/guilhermesantana/rede_social/controller/CommentController.java
@@ -4,10 +4,10 @@ import com.guilhermesantana.rede_social.domain.Comment;
 import com.guilhermesantana.rede_social.dtos.CommentDto;
 import com.guilhermesantana.rede_social.service.CommentService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -24,5 +24,17 @@ public class CommentController {
     public ResponseEntity<CommentDto>  createComment(@PathVariable("userid")UUID userId, @PathVariable("postid") UUID postId, @RequestBody CommentDto data) throws Exception{
         CommentDto newComment = this.commentService.createComment(userId, postId, data);
         return new ResponseEntity<>(newComment, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<Comment>> findCommentByUserId(@PathVariable("id") UUID id) throws Exception{
+        List<Comment> commentList = this.commentService.findCommentByUserId(id);
+        return new ResponseEntity<>(commentList, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CommentDto>> findAll() throws Exception{
+        List<CommentDto> commentList = this.commentService.findAll();
+        return new ResponseEntity<>(commentList, HttpStatus.OK);
     }
 }
