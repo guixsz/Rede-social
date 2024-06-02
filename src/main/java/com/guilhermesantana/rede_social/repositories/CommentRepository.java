ff24de505ec3eceac26b4887efb370e14aa447e6
Guilherme Santana <guiguilsantana@gmail.com>
2024-06-02 17:20:42 -0300

    feat: Added method findAllComments in CommentRepository

--- src/main/java/com/guilhermesantana/rede_social/repositories/CommentRepository.java
+++ src/main/java/com/guilhermesantana/rede_social/repositories/CommentRepository.java
@@ -1,9 +1,12 @@
 package com.guilhermesantana.rede_social.repositories;
 
 import com.guilhermesantana.rede_social.domain.Comment;
+import com.guilhermesantana.rede_social.dtos.CommentDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,4 +14,9 @@ import java.util.UUID;
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
 
     Optional<Comment> findCommentById(UUID id);
+
+    Optional<List<Comment>> findCommentsByUserId(UUID userId);
+
+    @Query("SELECT new com.guilhermesantana.rede_social.dtos.CommentDto(c.id, c.date, c.content, c.user.id, c.post.id) FROM Comment c")
+    List<CommentDto> findAllComments();
 }
